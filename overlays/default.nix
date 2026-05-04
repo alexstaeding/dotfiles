0c216304546976a640672d03a1bf2a47cabcb35a
Alexander Städing <alex@staeding.com>
2026-05-04 14:08:00 +0200

    Simplify default overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,9 @@
 { inputs }:
 final: prev:
-let
-  internalPkgs = import ../pkgs {
-    pkgs = final;
-    inherit inputs;
-  };
-in internalPkgs
+import ../pkgs {
+  pkgs = final;
+  inherit inputs;
+}
 // {
   algotex = inputs.algotex.packages.${final.stdenv.hostPlatform.system}.default;
   zed-latest = inputs.zed.packages.${final.stdenv.hostPlatform.system}.default;
