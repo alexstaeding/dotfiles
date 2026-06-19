a441a8a38107a686eedefc68562752e1c3970739
Alexander Städing <alex@staeding.com>
2026-06-19 14:34:49 -0400

    Add support for multiple users in cli-standalone host configuration

--- modules/hosts/cli-standalone/default.nix
+++ modules/hosts/cli-standalone/default.nix
@@ -1,24 +1,42 @@
-{ inputs, config, ... }:
+{
+  inputs,
+  config,
+  withSystem,
+  ...
+}:
+let
+  users = [
+    "alex"
+  ];
+in
 {
   flake.homeConfigurations = builtins.listToAttrs (
-    map (system: {
-      name = "alex@${system}";
-      value = config.withSystem system ({ pkgs, ... }:
-        inputs.home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = {
-            inherit inputs pkgs;
-            nixCats = inputs.nixCats;
-            inherit (inputs.self) outputs;
-          };
-          modules = [
-            { programs.home-manager.enable = true; }
-            config.flake.modules.homeManager.alex
-            config.flake.modules.homeManager.nano
-            config.flake.modules.homeManager.vim
-          ];
-        }
-      );
-    }) config.systems
+    builtins.concatLists (
+      map (
+        user:
+        map (system: {
+          name = "${user}@${system}";
+          value = withSystem system (
+            { pkgs, ... }:
+            inputs.home-manager.lib.homeManagerConfiguration {
+              inherit pkgs;
+              extraSpecialArgs = {
+                inherit inputs pkgs;
+                nixCats = inputs.nixCats;
+                inherit (inputs.self) outputs;
+              };
+              modules = [
+                { programs.home-manager.enable = true; }
+                config.flake.modules.homeManager.${user}
+                config.flake.modules.homeManager.dev-base
+                config.flake.modules.homeManager.lean
+                config.flake.modules.homeManager.nano
+                config.flake.modules.homeManager.vim
+              ];
+            }
+          );
+        }) config.systems
+      ) users
+    )
   );
 }
